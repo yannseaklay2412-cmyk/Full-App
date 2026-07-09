cf423454ad55e9609b8188f98341fa548068df15
Test User <test@example.com>
2026-07-09 23:36:11 +0700

    fix  email

--- Backend/services/email.service.js
+++ Backend/services/email.service.js
@@ -2,8 +2,8 @@ import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_PASS,
